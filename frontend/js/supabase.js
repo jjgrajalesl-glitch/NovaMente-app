fcedcf8158316af45dc58b77d09d3d5b7e2e1946
jjgrajalesl-glitch <jj.grajalesl@gmail.com>
2026-07-15 16:25:44 -0500

    Update supabase.js

--- frontend/js/supabase.js
+++ frontend/js/supabase.js
@@ -1,8 +1,15 @@
-const SUPABASE_URL = "https://vouoxlnggfbfivrfuwjz.supabase.co";
+// ==========================================
+// NovaMente
+// Supabase Client
+// ==========================================
 
-const SUPABASE_ANON_KEY = "sb_publishable_yhkPdbQ5yISsoXLxctYTwQ_-Tu3_mWt";
+const SUPABASE_URL =
+"https://vouoxlnggfbfivrfuwjz.supabase.co";
 
-const supabase = window.supabase.createClient(
+const SUPABASE_ANON_KEY =
+"sb_publishable_yhkPdbQ5yISsoXLxctYTwQ_-Tu3_mWt";
+
+window.novaSupabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
